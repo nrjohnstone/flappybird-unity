9a94256d1b309780cb30edb4a5fd17521920503a
nrjohnstone <locotewz@gmail.com>
2017-01-14 13:59:33 +0100

    completed part 3 of tutorial

---
+++ Assets/Scripts/Bird.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class Bird : MonoBehaviour {
+        public float upForce = 200f;
+        private bool isDead = false;
+        
+        private Rigidbody2D rb2d;
+
+        public void Start()
+        {
+            rb2d = GetComponent<Rigidbody2D>();
+        }
+
+        public void Update()
+        {
+            if (isDead == false)
+            {
+                if (Input.GetMouseButtonDown(0))
+                {
+                    rb2d.velocity = Vector2.zero;
+                    rb2d.AddForce(new Vector2(0, upForce));
+                }
+            }
+        }
+
+        public void OnCollisionEnter2D(Collision2D other)
+        {
+            rb2d.velocity = Vector2.zero;
+            isDead = true;
+        }
+    }
+}
