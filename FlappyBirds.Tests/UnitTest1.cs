8be8e26b138a98c6f11cff54a99edfb648e03518
nrjohnstone <locotewz@gmail.com>
2017-01-14 17:48:57 +0100

    add unit test project

---
+++ FlappyBirds.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Assets.Scripts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnityEngine;
+
+namespace FlappyBirds.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            
+        }
+    }
+}
